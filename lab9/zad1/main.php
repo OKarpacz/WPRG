c799cd2e403566d95eb85da1e914e2b6f47ccfa9
OKarpacz <olgierdkarpacz2000@gmail.com>
2024-05-20 10:50:56 +0200

    adding lab 9

---
+++ lab9/zad1/main.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_GET["submit"])) {
+    $birthDate = $_GET["date"];
+    $birthDateTime = new DateTime($birthDate);
+    $currentDate = new DateTime();
+
+    $dayOfTheWeek = date("N", strtotime($birthDate));
+    $userAge = $currentDate->diff($birthDateTime)->y;
+
+    $currentYear = $currentDate->format('Y');
+    $birthMonthAndDay = $birthDateTime->format('m-d');
+    $nextBirthday = new DateTime($currentYear . '-' . $birthMonthAndDay);
+
+    if ($nextBirthday > $currentDate) {
+        $nextBirthday = $nextBirthday->modify('+1 year');
+    }
+
+    $numberOfDaysToNextBirthday = $currentDate->diff($nextBirthday)->days;
+
+    $daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    $dayOfWeekName = $daysOfWeek[$dayOfTheWeek - 1];
+
+    echo "Born on $birthDate.<br>The day of the week was $dayOfWeekName.<br>
+    Your next birthday is in $numberOfDaysToNextBirthday days.<br>
+    You are $userAge years old.";
+}
+?>
